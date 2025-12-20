62ab8e2aade48c62da591164dc57871d1de88b6b
Bahrom Zokirov <bahromzokirov820@gmail.com>
2025-12-20 10:25:20 +0500

    Implement JWT authentication and authorization for ItemsController

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MassTransit;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Play.Catalog.Contracts;
 using Play.Catalog.Service.Entities;
@@ -12,6 +13,7 @@ namespace Play.Catalog.Service.Controllers;
 
 [ApiController]
 [Route("items")]
+[Authorize]
 public class ItemsController : ControllerBase
 {
     private readonly IRepository<Item> _repository;
--- Program.cs
+++ Program.cs
@@ -1,11 +1,31 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Play.Catalog.Service.Entities;
 using Play.Common.MassTransit;
 using Play.Common.MongoDB;
+using Play.Common.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// var allowedOrigin = builder.Configuration
+//     .GetSection("AllowedOrigin")
+//     .Get<string[]>();
+
+
+var allowedOrigin = builder.Configuration["AllowedOrigin"];
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.WithOrigins(allowedOrigin!)
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
@@ -13,11 +33,25 @@ builder.Services.AddControllers(options =>
     options.SuppressAsyncSuffixInActionNames = false
 );
 
-builder.Services.AddMongo()
+    var serviceSettings = builder.Configuration
+        .GetSection("ServiceSettings")
+        .Get<ServiceSettings>();
+
+
+builder.Services.AddMongo(builder.Configuration)
     .AddMongoRepository<Item>("items")
     .AddMassTransitWithRabbitMq();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://localhost:5003";
+        options.Audience = serviceSettings.ServiceName;
+    });
+
 var app = builder.Build();
+
+app.UseCors("CorsPolicy");
 
 if (app.Environment.IsDevelopment())
 {
@@ -31,6 +65,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
